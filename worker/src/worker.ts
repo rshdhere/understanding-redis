8bdcf697b1281eab40e3de96cbf42ce3ba742a01
raashed <mohammedraashed10@gmail.com>
2025-10-04 09:47:49 +0530

    chore: done

--- worker/src/worker.ts
+++ worker/src/worker.ts
@@ -1,1 +1,15 @@
-console.log('hello from the worker');
+import { createClient } from "redis";
+
+const client = createClient();
+
+async function main(){
+    await client.connect()
+    while(1){
+        const response = await client.brPop("submissions", 0);
+        console.log(response);
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        console.log('processed user submission')
+    }
+}
+
+main();
